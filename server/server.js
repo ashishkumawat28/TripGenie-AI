f4a661450fb8662395ac2aac24f9578761663de8
Ashish Kumawat <kumawatashish977@gamil.com>
2026-08-14 00:38:27 +0530

    Configure Vercel SPA routing

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://tripgenie-ai-alpha.vercel.app",
+      "http://voyaraai.vercel.app",
     ],
     credentials: true,
   })
